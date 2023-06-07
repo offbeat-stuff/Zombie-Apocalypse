2f016de70e7fae54d99d8897179e9a921ef77187
offbeat-stuff <sbteraparsecs@gmail.com>
2023-06-07 04:57:52 +0000

    well lets try potion effect parsing

--- src/main/java/io/github/offbeat_stuff/zombie_apocalypse/PotionEffectHandler.java
+++ src/main/java/io/github/offbeat_stuff/zombie_apocalypse/PotionEffectHandler.java
@@ -2,11 +2,16 @@ package io.github.offbeat_stuff.zombie_apocalypse;
 
 import static io.github.offbeat_stuff.zombie_apocalypse.ProbabilityHandler.tryChance;
 
+import java.util.List;
+
 import io.github.offbeat_stuff.zombie_apocalypse.config.ConfigHandler;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.registry.Registries;
+
+import static net.minecraft.util.math.MathHelper.clamp;
 
 public class PotionEffectHandler {
 
@@ -39,4 +44,32 @@ public class PotionEffectHandler {
         effect, ZombieMod.XRANDOM.nextInt(ConfigHandler.maxPotionTimeInTicks),
         ZombieMod.XRANDOM.nextInt(ConfigHandler.maxAmplifier) + 1));
   }
-}
+
+  public static class RawStatusEffectHandler {
+    public List<String> effects = List.of();
+    public int maxTimeInTicks;
+    public List<Float> incrementalChances;
+
+    public RawStatusEffectHandler(List<String> effects, int maxTimeInTicks,List<Float> incrementalChances) {
+      this.effects = effects;
+      this.maxTimeInTicks = maxTimeInTicks;
+      this.incrementalChances = incrementalChances;
+    }
+
+    private String fixStatusEffect(List<String> IDS,String effect) {
+      effect = effect.trim().toLowerCase();
+      for (int i = 0;i < IDS.size();i++) {
+        if (effect.startsWith(IDS.get(i)) {
+          return IDS.get(i);
+        }
+      }
+      return "";
+    }
+
+    public void fixAll() {
+      var IDS = Registries.STATUS_EFFECT.getIds().stream().map(f -> f.getPath()).toList();
+      this.effects = this.effects.stream().map(f -> fixStatusEffect(IDS,f)).filter(f -> !f.equals("")).toList();
+      this.maxTimeInTicks = Math.max(this.maxTimeInTicks, -1);
+      this.incrementalChances = this.incrementalChances.stream().map(f -> clamp(f, 0f, 1f)).toList();
+    }
+  }}
