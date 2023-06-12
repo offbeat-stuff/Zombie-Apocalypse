f4d44c4f44c95f43c7e8c09dbb72ed75d749e517
Artix Kai <artix@github.com>
2023-06-12 17:09:16 +0530

    well some publishing changes

--- publish.sh
+++ publish.sh
@@ -3,3 +3,11 @@ nim changelog/bumpVersion.nims
 git add gradle.properties
 git commit -m "bump Version"
 gradle modrinth
+
+patch gradle.properties -i patches/gradle.properties.patch
+patch src/main/resources/fabric.mod.json -i patches/fabric.mod.json.patch
+patch src/main/java/io/github/offbeat_stuff/zombie_apocalypse/ArmorTrimHander.java -i patches/ArmorTrimHander.java.patch
+gradle modrinth
+patch gradle.properties -i patches/gradle.properties.patch --reverse
+patch src/main/resources/fabric.mod.json -i patches/fabric.mod.json.patch --reverse
+patch src/main/java/io/github/offbeat_stuff/zombie_apocalypse/ArmorTrimHander.java -i patches/ArmorTrimHander.java.patch --reverse
