6fa4affbae55225e49ca90c8f3a652fbc6f2b07a
ZenXArch mobile <offbeat-stuff@github.com>
2023-06-24 16:11:59 +0530

    some fixes

--- publish.sh
+++ publish.sh
@@ -6,8 +6,8 @@ gradle modrinth
 
 patch gradle.properties -i patches/gradle.properties.patch
 patch src/main/resources/fabric.mod.json -i patches/fabric.mod.json.patch
-patch src/main/java/io/github/offbeat_stuff/zombie_apocalypse/ArmorTrimHander.java -i patches/ArmorTrimHander.java.patch
+patch src/main/java/io/github/offbeat_stuff/zombie_apocalypse/ArmorTrimHandler.java -i patches/ArmorTrimHandler.java.patch
 gradle modrinth
 git restore gradle.properties
 git restore src/main/resources/fabric.mod.json
-git restore src/main/java/io/github/offbeat_stuff/zombie_apocalypse/ArmorTrimHander.java
+git restore src/main/java/io/github/offbeat_stuff/zombie_apocalypse/ArmorTrimHandler.java
