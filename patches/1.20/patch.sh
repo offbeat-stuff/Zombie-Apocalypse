f423d8984f0ddd3b3f8949bf418066b339465d4b
Artix Kai <artix@github.com>
2023-06-26 21:25:07 +0530

    1.20 patches

---
+++ patches/1.20/patch.sh
@@ -0,0 +1,5 @@
+PATCH_HOME="patches/1.20/"
+
+patch gradle.properties -i $PATCH_HOME/gradle.properties.patch
+patch src/main/resources/fabric.mod.json -i $PATCH_HOME/fabric.mod.json.patch
+patch src/main/java/io/github/offbeat_stuff/zombie_apocalypse/config/ArmorTrimHandler.java -i $PATCH_HOME/ArmorTrimHandler.java.patch
