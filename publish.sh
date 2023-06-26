9b72bc649e9a6771a28318b4b9279d7e7d91977f
Artix Kai <artix@github.com>
2023-06-26 21:24:42 +0530

    rearrange publish script

--- publish.sh
+++ publish.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 gradle modrinth
 
-patch gradle.properties -i patches/gradle.properties.patch
-patch src/main/resources/fabric.mod.json -i patches/fabric.mod.json.patch
-patch src/main/java/io/github/offbeat_stuff/zombie_apocalypse/ArmorTrimHandler.java -i patches/ArmorTrimHandler.java.patch
+patches/1.19/patch.sh
 gradle modrinth
 git restore gradle.properties
-git restore src/main/resources/fabric.mod.json
-git restore src/main/java/io/github/offbeat_stuff/zombie_apocalypse/ArmorTrimHandler.java
+git restore src/main
+
+patches/1.20/patch.sh
+gradle modrinth
+git restore gradle.properties
+git restore src/main
