47413126f6ef841c1837af11a2197c85c4264603
ZenXArch mobile <offbeat-stuff@github.com>
2023-07-31 08:57:41 +0530

    feat: sound when screaming zombie message

--- src/main/java/io/github/offbeat_stuff/zombie_apocalypse/ScreamHandler.java
+++ src/main/java/io/github/offbeat_stuff/zombie_apocalypse/ScreamHandler.java
@@ -1,7 +1,11 @@
 package io.github.offbeat_stuff.zombie_apocalypse;
 
+import static io.github.offbeat_stuff.zombie_apocalypse.ZombieMod.XRANDOM;
+
+import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
 import net.minecraft.network.packet.s2c.play.TitleS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
@@ -16,5 +20,9 @@ public class ScreamHandler {
     }
     player.networkHandler.sendPacket(new TitleS2CPacket(
         Text.literal("Zombies Are Coming").formatted(Formatting.DARK_RED)));
+    var v = player.getEyePos().toVector3f();
+    player.networkHandler.sendPacket(new PlaySoundS2CPacket(
+        VersionDependent.getSound("entity.zombie.ambient"),
+        SoundCategory.HOSTILE, v.x, v.y, v.z, 2.0f, 1.0f, XRANDOM.nextLong()));
   }
 }
